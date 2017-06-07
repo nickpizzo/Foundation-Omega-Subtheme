27dfb2b3c2d9cf66700fc51ccbe6bc97e129f4d9
Nick Pizzo <nick.pizzo@gmail.com>
2017-06-07 10:44:54 -0700

    added fontawesome, uses minified foundation js

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -96,22 +96,13 @@
     </div>
 
     <!-- Mobile Nav Hidden Menu -->
-    <div class="off-canvas position-left" id="offCanvas" data-off-canvas>
+    <div class="off-canvas position-top" id="offCanvas" data-off-canvas>
       <!-- Close button -->
       <button class="close-button" aria-label="Close menu" type="button" data-close>
         <span aria-hidden="true">&times;</span>
       </button>
       <!-- Menu -->
-      <div class="super">
-      <ul class="vertical menu">
-        <li><a href="#">Foundation</a></li>
-        <li><a href="#">Dot</a></li>
-        <li><a href="#">ZURB</a></li>
-        <li><a href="#">Com</a></li>
-        <li><a href="#">Slash</a></li>
-        <li><a href="#">Sites</a></li>
-      </ul>
-      </div>
+      <?php print render($page['navigation']); ?>
 
     </div>
 
